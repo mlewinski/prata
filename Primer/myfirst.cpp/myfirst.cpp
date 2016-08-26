edd184cde8e3a515e1ff66578e1023f820b3848b
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-26 16:11:48 +0200

    Implemented myfirst.cpp example (p48)

---
+++ Primer/myfirst.cpp/myfirst.cpp
@@ -0,0 +1,17 @@
+// myfirst.cpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "Play with C++ language!";
+	cout << endl;
+	cout << "New line" << endl;
+	cin.get();
+	return 0;
+}
+
