669d694412a8a9702ba88817594e50aad609106c
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-26 16:49:25 +0200

    Implemented carrots.cpp example (p59)

---
+++ Primer/carrots/carrots.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+	int carrots = 25;
+	cout << "I have " << carrots << " carrots" << endl;
+	carrots = carrots - 1;
+	cout << "Now I have " << carrots << " carrots" << endl;
+	cin.get();
+	return 0;
+}
