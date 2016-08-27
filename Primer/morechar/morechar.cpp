cf55e9d9fa5ff5ab673c2595f0a44d9c3ad282df
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-27 21:20:04 +0200

    Implemented char and morechar examples (p92-93)

---
+++ Primer/morechar/morechar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Input char :" << endl;
+	char ch;
+	cin >> ch;
+	int ascii = ch;
+	cout << ch << " : " << ascii << " dec ";
+	cout << oct;
+	cout << ascii << " oct ";
+	cout << hex;
+	cout << ascii << " hex ";
+	cin.get();
+	cin.get();
+	return 0;
+}
