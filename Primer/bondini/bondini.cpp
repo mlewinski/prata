da68d19a073d27255ba47ec3db348956594afe90
Marek Lewiński <lewinski.marek@outlook.com>
2016-08-29 22:13:27 +0200

    Implemented special characters example (p97)

---
+++ Primer/bondini/bondini.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	using namespace std;
+	cout << "Insert your code : ______\b\b\b\b\b\b";
+	long code;
+	cin >> code;
+	cout << "You have entered " << code << " as your code" << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}
